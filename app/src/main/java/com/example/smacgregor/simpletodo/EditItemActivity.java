66eb504356246d580c1ae35d3951cd4de21a0779
Scott MacGregor <smacgregor@zynga.com>
2016-01-26 23:37:17 -0800

    Add the ability to edit an existing to-do item in the list. The submit button on the edit activity will only be enabled when the value of the to-do is not empty.

--- app/src/main/java/com/example/smacgregor/simpletodo/EditItemActivity.java
+++ app/src/main/java/com/example/smacgregor/simpletodo/EditItemActivity.java
@@ -1,16 +1,36 @@
 package com.example.smacgregor.simpletodo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditItemActivity extends AppCompatActivity {
+
+    // Move these to a shared constants file
+    public static final String kToDosItemPosition = "ToDoPosition";
+    public static final String kToDosItemValue = "ToDoValue";
+
+    private EditText editItemTextField;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_item);
+
+        saveButton = (Button)findViewById(R.id.saveButton);
+        saveButton.setEnabled(false); // can this be specified in XML?
+        editItemTextField = (EditText)findViewById(R.id.editText);
+
+        setupEditItemTextField(getIntent().getStringExtra(kToDosItemValue));
     }
 
     @Override
@@ -33,5 +53,31 @@ public class EditItemActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onSubmitItemModification(View view) {
+        saveModifications();
+    }
+
+    private void saveModifications() {
+        Intent modifiedData = new Intent();
+        modifiedData.putExtra(kToDosItemValue, editItemTextField.getText().toString());
+        modifiedData.putExtra(kToDosItemPosition, getIntent().getIntExtra(kToDosItemPosition, 0));
+        setResult(RESULT_OK, modifiedData);
+        finish();
+
+    }
+
+    private void setupEditItemTextField(final String editItemName) {
+        editItemTextField.append(editItemName);
+
+        // Only enable the save button when we have a non empty todo item name
+        editItemTextField.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                saveButton.setEnabled(!TextUtils.isEmpty(s.toString()));
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        });
     }
 }
